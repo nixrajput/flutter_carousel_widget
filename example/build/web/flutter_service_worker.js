2479f37432bc55e52d98b459f7f57aa0aa753955
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-10-06 20:15:15 +0530

    updated to version 1.2.1

--- example/build/web/flutter_service_worker.js
+++ example/build/web/flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1bc5612e8bef7003ced6098e2092aa5a",
-"/": "1bc5612e8bef7003ced6098e2092aa5a",
+"index.html": "0b20f2fee5cfe04aa1d8dd65ea37e063",
+"/": "0b20f2fee5cfe04aa1d8dd65ea37e063",
 "main.dart.js": "f0101c721a0c696af2fde70f6d3c0cea",
 "manifest.json": "94564f4b9cbcfb4f0ba3d013b526d29b",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
